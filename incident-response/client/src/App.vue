64c51746250a9b5965e4b763fce8d41d0e0aa937
josh <motojosh2@gmail.com>
2020-08-17 21:17:33 -0400

    Prep for visualization on client side

--- incident-response/client/src/App.vue
+++ incident-response/client/src/App.vue
@@ -1,40 +1,52 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-toolbar-title>Incident Response</v-toolbar-title>
-      </div>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Incident Response</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <router-view />
     </v-main>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Account</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
   },
 
   data: () => ({
-    //
+    drawer: false
   })
 };
 </script>
---
+++ incident-response/client/src/components/Map.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <p>Map goes here</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Map",
+
+  data: () => ({
+  })
+};
+</script>
